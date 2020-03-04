132630a6d7be15d9ba6c1e4075a95d02352afb8e
Pavel Kulesh <kulesh.p.v.b.2.m@gmail.com>
2020-03-04 21:49:39 +0300

    add brackets solition

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const array = str.split('');
+  for (let i = 0; i < str.length / 2; i += 1) {
+    for (let j = 0; j < str.length; j += 1) {
+      for (let k = 0; k < bracketsConfig.length; k += 1) {
+        if (array[j] === bracketsConfig[k][0] && array[j + 1] === bracketsConfig[k][1]) {
+          array.splice(j, 2);
+        }
+      }
+    }
+  }
+  return array.length === 0;
+};
